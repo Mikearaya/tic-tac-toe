060bc0d10dd4068e3c4fce1c6b9ec12257766ed9
mikearaya <mikaelaraya12@gmail.com>
2020-05-04 12:39:32 +0300

    Refactor game logic from main.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,38 @@
 require_relative './player.rb'
+require_relative './board.rb'
 class Game
   attr_reader :total_match
   def initialize
     @total_match = 0
     @players = []
     @current_round = 0
-
+    @board = Board.new
     display_header
   end
 
   def start
     add_players
+    continue = true
+    while continue
+      turn = 1
+      while round < 9
+        @board.draw_board
+        turn = turn == 2 ? 1 : 2
+        puts get_player(turn).name + '\'s Turn'
+        choice = user_move
+        @board.mark_tile(choice, get_player(turn).symbole)
+        update_round
+
+        next unless @board.won?(get_player(turn).symbole)
+
+        display_winner_message(turn)
+        break
+      end
+      puts 'Do you want to play another round (y/n)'
+      play = gets.chomp
+      continue = play.downcase != 'n'
+      @board.reset_board
+    end
   end
 
   private
@@ -29,8 +51,26 @@ class Game
     @players << Player.new(second_player_name, second_player_symbole)
   end
 
+  def update_round
+    @current_round += 1
+  end
+
   def get_player(index)
-    @players[index]
+    @players[index - 1]
+  end
+
+  def validate_move(choice)
+    choice.match(/[1-9]/)
+  end
+
+  def user_move
+    choice = false
+    unless choice
+      choice = gets.chomp
+      choice = validate_move(choice) ? choice.to_i : false
+      puts 'invalid input \n valid input is a number between 1 and 9' unless choice
+    end
+    choice
   end
 
   def display_header
@@ -40,6 +80,15 @@ class Game
       *           TIC TAC TOE               *
       *                                     *
       ***************************************
+    HEARDOC
+  end
+
+  def display_winner_message(winner)
+    <<-HEARDOC
+    ************************************************************
+    *                    Congradulation                        *
+    *          #{get_player(winner).name}  has won             *
+    ************************************************************
     HEARDOC
   end
 end
